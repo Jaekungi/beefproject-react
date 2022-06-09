98b6be9d48eb6b1d78bdaa2797b42b1d46a0c23b
Jaekungi <chayaphon.ma@gmail.com>
2022-06-09 21:30:17 +0700

    first commit

---
+++ src/components/auth/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  console.log("Header");
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>
+            BeefLover
+          </Link>
+          <div className="d-flex justify-content-end">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/auth/LoginComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+export default class Login extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Login</h3>
+        <div className="mb-3">
+          <label>UserName</label>
+          <input
+            type="username"
+            className="form-control"
+            placeholder="Enter username"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="mb-3"></div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
